4ef3c88c96c1eb581b4adf3c0c00d466d6f6a224
adibaahsan04080 <adibaahsan04080@gmail.com>
2026-03-03 10:30:40 +0600

    constructor

--- src/Product.java
+++ src/Product.java
@@ -5,3 +5,14 @@ public class Product {
     public String name;
     public int price;
     public int stock;
+
+        public Product(String productId, String category, String brand, String name, int price, int stock) {
+        this.productId = productId;
+        this.category = category;
+        this.brand = brand;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
+
