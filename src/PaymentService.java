7448155a2373e68a53907d4f6dab5057c6e45bf2
tasnim240 <maliha24755@gmail.com>
2026-03-03 22:46:32 +0600

    Added conditon in PaymentService.java

--- src/PaymentService.java
+++ src/PaymentService.java
@@ -8,3 +8,14 @@ public class PaymentService {
  this.log = log;
  }
  
+
+ /** Process payment for an order. Returns true if payment succeeds.
+*/
+ public boolean processPayment(Order order, BufferedReader console)
+throws Exception {
+ String mode = safe(order.paymentMode);
+ if (mode.equalsIgnoreCase("COD")) {
+ // Cash on Delivery always "approved" (no upfront failure)
+ log.write(order.orderId, "PAYMENT OK (Cash on Delivery)");
+ return true;
+ }
